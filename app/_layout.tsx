e564f6fc5706712630a14bf97d9e8914a69035fb
jbreite <joshbreite@gmail.com>
2024-09-15 16:38:46 -0400

    Opening and closing modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <GestureHandlerRootView>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Animated, { SlideInDown, SlideOutDown } from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const SCREEEN_WIDTH = Dimensions.get("window").width;
+const HORIZONTAL_MARGIN = 24;
 
 export default function Index() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { bottom } = useSafeAreaInsets();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Animated.View style={styles.mainContainer}>
+      <TouchableOpacity onPress={() => setIsOpen((prev) => !prev)}>
+        <Text>Open Modal</Text>
+      </TouchableOpacity>
+
+      {isOpen && (
+        <Animated.View
+          style={{
+            backgroundColor: "red",
+            position: "absolute",
+            bottom: bottom,
+            left: HORIZONTAL_MARGIN,
+            right: HORIZONTAL_MARGIN,
+            padding: 24,
+            borderRadius: 24,
+            borderCurve: "continuous",
+          }}
+          entering={SlideInDown}
+          exiting={SlideOutDown}
+        >
+          <View style={{ flex: 1, gap: 12 }}>
+            <Text>View Private Key</Text>
+            <Text>View Recovery Phrase</Text>
+            <Text>Remove Wallet</Text>
+          </View>
+        </Animated.View>
+      )}
+    </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+});
